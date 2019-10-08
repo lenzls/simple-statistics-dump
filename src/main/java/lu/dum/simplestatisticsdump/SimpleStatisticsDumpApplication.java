b722bfe5f6f3e5042fdf2c550ccc90d9cc821743
Simon Lenz <dev@simon-lenz.de>
2019-10-09 01:42:48 +0200

    spring initializer did what he's figuratively paid for

---
+++ src/main/java/lu/dum/simplestatisticsdump/SimpleStatisticsDumpApplication.java
@@ -0,0 +1,13 @@
+package lu.dum.simplestatisticsdump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStatisticsDumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStatisticsDumpApplication.class, args);
+	}
+
+}
